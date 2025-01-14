63fa246f3d1d4b8d7759324d6cc1bd22b56f2f81
W <william.perron@polymtl.ca>
2025-01-14 15:04:53 -0500

    feat : Ajout tools et ajout grille

--- client/src/app/pages/edition-page/edition-page.component.ts
+++ client/src/app/pages/edition-page/edition-page.component.ts
@@ -1,8 +1,33 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-edition-page',
     templateUrl: './edition-page.component.html',
     styleUrls: ['./edition-page.component.scss'],
+    imports: [CommonModule, FormsModule],
 })
-export class EditionPageComponent {}
+export class EditionPageComponent {
+    activeTool: string | null = null;
+    grid: string[][];
+    gameName: string = '';
+    gameDescription: string = '';
+
+    constructor() {
+        this.grid = this.generateGrid(10, 10); // Générer une grille 10x10 par défaut
+    }
+
+    selectTool(tool: string): void {
+        this.activeTool = tool;
+    }
+
+    isToolActive(tool: string): boolean {
+        return this.activeTool === tool;
+    }
+
+    private generateGrid(rows: number, cols: number): string[][] {
+        return Array.from({ length: rows }, () => Array(cols).fill(''));
+    }
+}
