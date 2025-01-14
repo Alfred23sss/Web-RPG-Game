3f7e69912c498685cff971570bbe9a92a9eee657
R3ivil <olivierohara@outlook.com>
2025-01-14 15:48:54 -0500

    pop-up

--- client/src/app/pages/admin-page/admin-page.component.ts
+++ client/src/app/pages/admin-page/admin-page.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { RouterLink } from '@angular/router';
+import { PopUpComponent } from '@app/components/pop-up/pop-up.component';
+
 
 @Component({
     selector: 'app-admin-page',
@@ -7,4 +10,11 @@ import { RouterLink } from '@angular/router';
     styleUrls: ['./admin-page.component.scss'],
     imports: [RouterLink],
 })
-export class AdminPageComponent {}
+export class AdminPageComponent {
+
+    constructor(private dialogRef: MatDialog) {}
+
+    openDialog(){(
+        this.dialogRef.open(PopUpComponent));
+    }
+}
