b22c263c6ca708a0ef7d69f61ad7385567cc0a3f
R3ivil <olivierohara@outlook.com>
2025-01-19 14:14:26 -0500

    lint fix

--- client/src/app/components/pop-up/pop-up.component.ts
+++ client/src/app/components/pop-up/pop-up.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-pop-up',
-  imports: [],
-  templateUrl: './pop-up.component.html',
-  styleUrl: './pop-up.component.scss'
+    selector: 'app-pop-up',
+    imports: [],
+    templateUrl: './pop-up.component.html',
+    styleUrl: './pop-up.component.scss',
 })
-export class PopUpComponent {
-
-}
+export class PopUpComponent {}
