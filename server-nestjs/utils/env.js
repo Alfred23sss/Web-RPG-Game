18b38b8fa178157dcad738f978fdf22da86b387e
R3ivil <olivierohara@outlook.com>
2025-01-21 15:22:26 -0500

    Trying to install DB

---
+++ server-nestjs/utils/env.js
@@ -0,0 +1,8 @@
+const DB_DB = "LOG2990_PROJECT";
+const DB_COLLECTIONS_GAMESTORAGE = "GameStorage";
+
+const DB_URL = "mongodb+srv://olivierohara:Pk0iTlpfp3TDaB8h@cluster0.t938x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+exports.DB_DB = DB_DB;
+exports.DB_URL = DB_URL;
+exports.DB_COLLECTIONS_GAMESTORAGE = DB_COLLECTIONS_GAMESTORAGE;
