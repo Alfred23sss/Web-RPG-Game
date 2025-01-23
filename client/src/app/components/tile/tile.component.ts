47e89bad67702238067650a7a411a387d3344d07
Alfred Galibois-Bouchard <alfredgaliboisb@gmail.com>
2025-01-23 14:30:03 -0500

    fix bug right click + fonctionnalite delete and drag

--- client/src/app/components/tile/tile.component.ts
+++ client/src/app/components/tile/tile.component.ts
@@ -11,6 +11,7 @@ import { ToolService } from '@app/services/tool.service';
 })
 export class TileComponent {
     static isDragging = false;
+    static isRightClicking = false;
     @Input() tile!: Tile;
 
     constructor(
@@ -18,51 +19,53 @@ export class TileComponent {
         private gridService: GridService,
     ) {}
 
-    @HostListener('mousedown')
-    onMouseDown(): void {
-        TileComponent.isDragging = true;
-        this.applyTool();
+    @HostListener('mousedown', ['$event'])
+    onMouseDown(event: MouseEvent): void {
+        if (event.button === 2) {
+            TileComponent.isRightClicking = true;
+            this.removeTileType();
+        } else if (event.button === 0) {
+            TileComponent.isDragging = true;
+            this.applyTool();
+        }
     }
 
     @HostListener('mouseenter')
     onMouseEnter(): void {
         if (TileComponent.isDragging) {
             this.applyTool();
+        } else if (TileComponent.isRightClicking) {
+            this.removeTileType();
         }
     }
 
     @HostListener('contextmenu', ['$event'])
     onRightClick(event: MouseEvent): void {
         event.preventDefault();
-        this.removeTileType();
     }
 
     @HostListener('document:mouseup')
     onMouseUp(): void {
         TileComponent.isDragging = false;
+        TileComponent.isRightClicking = false;
     }
 
     private applyTool(): void {
         const selectedTool = this.toolService.getSelectedTool();
         if (selectedTool) {
             const [row, col] = this.tile.id.split('-').slice(1).map(Number);
-            // console.log(this.gridService.getTile(row, col).type);
             this.gridService.updateTile(row, col, {
                 imageSrc: selectedTool.image,
                 type: selectedTool.tool,
             });
-            // console.log(this.gridService.getTile(row, col).type);
         }
     }
 
     private removeTileType(): void {
-        const selectedTool = this.toolService.getSelectedTool();
-        if (selectedTool) {
-            const [row, col] = this.tile.id.split('-').slice(1).map(Number);
-            this.gridService.updateTile(row, col, {
-                imageSrc: ImageType.Default,
-                type: TileType.Default,
-            });
-        }
+        const [row, col] = this.tile.id.split('-').slice(1).map(Number);
+        this.gridService.updateTile(row, col, {
+            imageSrc: ImageType.Default,
+            type: TileType.Default,
+        });
     }
 }
