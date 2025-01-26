c09fec88087906a660d3c251ac429cffe46115b5
W <william.perron@polymtl.ca>
2025-01-26 13:38:28 -0500

    feat : cdkDrag

--- client/src/app/components/item-bar/item-bar.component.ts
+++ client/src/app/components/item-bar/item-bar.component.ts
@@ -1,12 +1,12 @@
+import { CdkDrag } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { ItemComponent } from '../item/item.component';
-
 @Component({
     selector: 'app-item-bar',
     templateUrl: './item-bar.component.html',
     styleUrls: ['./item-bar.component.scss'],
-    imports: [CommonModule],
+    imports: [CommonModule, CdkDrag],
 })
 export class ItemBarComponent {
     @Input() itemCount: number = 4;
