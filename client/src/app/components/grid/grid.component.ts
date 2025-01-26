1b398689b68cbf382c3bbbbfec36cab20dead51c
R3ivil <olivierohara@outlook.com>
2025-01-26 15:51:33 -0500

    fixed merge conflict

--- client/src/app/components/grid/grid.component.ts
+++ client/src/app/components/grid/grid.component.ts
@@ -12,32 +12,13 @@ import { ItemComponent } from '../item/item.component';
     templateUrl: './grid.component.html',
     styleUrls: ['./grid.component.scss'],
 })
+
 export class GridComponent {
     // @Input() rows: number;
     // @Input() cols: number;
     @Input() grid: Tile[][] | undefined = [];
-export class GridComponent implements OnInit {
-    @Input() rows: number = 10;
-    @Input() cols: number = 10;
-    grid: { id: string; imageSrc: string; isOccupied: boolean; type: string; isOpen: boolean }[][] = [];
-
-    ngOnInit() {
-        this.grid = this.generateGrid(this.rows, this.cols);
-    }
-
-    private generateGrid(rows: number, cols: number): any[][] {
-        return Array.from({ length: rows }, (_, rowIndex) =>
-            Array.from({ length: cols }, (_, colIndex) => ({
-                id: `tile-${rowIndex}-${colIndex}`,
-                imageSrc: 'assets/images/clay.png',
-                isOccupied: false,
-                type: 'default',
-                isOpen: true,
-            })),
-        );
-    }
-
     drop(event: CdkDragDrop<ItemComponent[]>): void {
         console.log(event);
       }
 }
+
