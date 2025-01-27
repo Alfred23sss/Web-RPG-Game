9d15f4b38820085238eb6a4cb8bf80e70cc8759a
TheLordSpoty <louisfelixbellemare@gmail.com>
2025-01-27 12:53:17 -0500

    fix: Popup component tests

--- client/src/app/pages/waiting-view/waiting-view.component.ts
+++ client/src/app/pages/waiting-view/waiting-view.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-waiting-view',
-  templateUrl: './waiting-view.component.html',
-  styleUrls: ['./waiting-view.component.scss'],
+    selector: 'app-waiting-view',
+    templateUrl: './waiting-view.component.html',
+    styleUrls: ['./waiting-view.component.scss'],
 })
 export class WaitingViewComponent implements OnInit {
-  accessCode: string;
+    accessCode: string;
 
-  ngOnInit() {
-    this.generateAccessCode();
-  }
+    ngOnInit() {
+        this.generateAccessCode();
+    }
 
-  generateAccessCode() {
-    this.accessCode = Math.floor(1000 + Math.random() * 9000).toString();
-  }
+    generateAccessCode() {
+        this.accessCode = Math.floor(1000 + Math.random() * 9000).toString();
+    }
 }
