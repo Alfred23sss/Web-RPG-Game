ee0f559e9858ebb11bacd6145bc11c456b0cb35a
TheLordSpoty <louisfelixbellemare@gmail.com>
2025-01-28 09:43:15 -0500

    feat/game-mode service tests

--- client/src/app/services/game-mode.service.spec.ts
+++ client/src/app/services/game-mode.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GameModeService } from './game-mode.service';
+import { GameMode, GameSize } from '@app/interfaces/game';
 
 describe('GameModeService', () => {
     let service: GameModeService;
@@ -12,5 +13,24 @@ describe('GameModeService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should initially have gameMode and gameSize set to None', () => {
+        expect(service.getGameMode()).toBe(GameMode.None);
+        expect(service.getGameSize()).toBe(GameSize.None);
+    });
+
+    it('should set and get gameMode correctly', () => {
+        const newGameMode = 'Classic';
+        service.setGameMode(newGameMode);
+
+        expect(service.getGameMode()).toBe(newGameMode);
+    });
+
+    it('should set and get gameSize correctly', () => {
+        const newGameSize = '15';
+        service.setGameSize(newGameSize);
+
+        expect(service.getGameSize()).toBe(newGameSize);
     });
 });
