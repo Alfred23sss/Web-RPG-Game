31274344caf4e8ec8e11d657076205041845ac41
TheLordSpoty <louisfelixbellemare@gmail.com>
2025-01-28 15:39:41 -0500

    fix: all tests currently written pass

--- client/src/app/components/tile/tile.component.spec.ts
+++ client/src/app/components/tile/tile.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TileComponent } from './tile.component';
+import { ToolService } from '@app/services/tool/tool.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ImageType, TileType } from '@app/interfaces/tile';
 
 describe('TileComponent', () => {
     let component: TileComponent;
@@ -8,10 +11,21 @@ describe('TileComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [TileComponent],
+            providers: [{ provide: ToolService, useValue: {} }],
+            schemas: [NO_ERRORS_SCHEMA],
         }).compileComponents();
 
         fixture = TestBed.createComponent(TileComponent);
         component = fixture.componentInstance;
+
+        component.tile = {
+            id: 'test_id',
+            type: TileType.Default,
+            imageSrc: ImageType.Default,
+            isOpen: false,
+            isOccupied: false,
+        };
+
         fixture.detectChanges();
     });
 
