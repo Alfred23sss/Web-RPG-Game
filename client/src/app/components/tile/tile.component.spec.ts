30cd23b9ed28a901033c3faecadb1b91879d85ce
W <william.perron@polymtl.ca>
2025-02-09 09:03:31 -0500

    feat : Tile test start

--- client/src/app/components/tile/tile.component.spec.ts
+++ client/src/app/components/tile/tile.component.spec.ts
@@ -1,35 +1,97 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { ImageType, ItemDescription, ItemType, Tile, TileType } from '@app/interfaces/tile';
+import { ItemDragService } from '@app/services/ItemDrag.service';
+import { TileService } from '@app/services/tile/Tile.service';
 import { TileComponent } from './tile.component';
-import { ToolService } from '@app/services/tool/tool.service';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { ImageType, TileType } from '@app/interfaces/tile';
 
 describe('TileComponent', () => {
     let component: TileComponent;
     let fixture: ComponentFixture<TileComponent>;
+    let tileServiceSpy: jasmine.SpyObj<TileService>;
+    let itemDragServiceSpy: jasmine.SpyObj<ItemDragService>;
+    let mockTile: Tile;
 
     beforeEach(async () => {
+        tileServiceSpy = jasmine.createSpyObj('TileService', ['removeTileObject', 'removeTileType', 'applyTool', 'drop']);
+        itemDragServiceSpy = jasmine.createSpyObj('ItemDragService', ['setSelectedItem']);
+
         await TestBed.configureTestingModule({
-            imports: [TileComponent],
-            providers: [{ provide: ToolService, useValue: {} }],
-            schemas: [NO_ERRORS_SCHEMA],
+            imports: [TileComponent], // Correction : TileComponent doit être dans imports car c'est un standalone component
+            providers: [
+                { provide: TileService, useValue: tileServiceSpy },
+                { provide: ItemDragService, useValue: itemDragServiceSpy },
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(TileComponent);
         component = fixture.componentInstance;
-
-        component.tile = {
+        mockTile = {
             id: 'test_id',
-            type: TileType.Default,
+            type: TileType.Ice,
             imageSrc: ImageType.Default,
             isOpen: false,
             isOccupied: false,
-        };
-
+            item: {
+                id: '0',
+                name: 'lightning',
+                imageSrc: ItemType.Lightning,
+                imageSrcGrey: ItemType.LightningGray,
+                itemCounter: 1,
+                description: ItemDescription.Lightning,
+            },
+        } as Tile;
+        component.tile = mockTile;
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should call selectObject when overlay image is clicked', () => {
+        const overlayImage = fixture.debugElement.query(By.css('.overlay-image'));
+        overlayImage.triggerEventHandler('mousedown', {});
+        expect(itemDragServiceSpy.setSelectedItem).toHaveBeenCalledWith(mockTile.item, mockTile);
+    });
+
+    it('should call applyTool on left click if no item is present', () => {
+        component.tile.item = undefined;
+        component.onMouseDown(new MouseEvent('mousedown', { button: 0 }));
+        expect(tileServiceSpy.applyTool).toHaveBeenCalledWith(mockTile);
+    });
+
+    it('should remove tile object on right click if item is present', () => {
+        component.onMouseDown(new MouseEvent('mousedown', { button: 2 }));
+        expect(tileServiceSpy.removeTileObject).toHaveBeenCalledWith(mockTile);
+    });
+
+    it('should remove tile type on right click if no item is present', () => {
+        component.tile.item = undefined;
+        component.onMouseDown(new MouseEvent('mousedown', { button: 2 }));
+        expect(tileServiceSpy.removeTileType).toHaveBeenCalledWith(mockTile);
+    });
+
+    it('should prevent context menu from opening', () => {
+        const event = new MouseEvent('contextmenu');
+        spyOn(event, 'preventDefault');
+        component.onRightClick(event);
+        expect(event.preventDefault).toHaveBeenCalled();
+    });
+
+    it('should reset activeButton on mouseup', () => {
+        TileComponent.activeButton = 0;
+        component.onMouseUp(new MouseEvent('mouseup', { button: 0 }));
+        expect(TileComponent.activeButton).toBeNull();
+    });
+
+    it('should set isDraggedTest to true on drag over', () => {
+        component.onDragOver(new DragEvent('dragover'));
+        expect(TileComponent.isDraggedTest).toBeTrue();
+    });
+
+    it('should call drop on drop event', () => {
+        component.onDrop(new DragEvent('drop'));
+        expect(tileServiceSpy.drop).toHaveBeenCalledWith(mockTile);
     });
 });
