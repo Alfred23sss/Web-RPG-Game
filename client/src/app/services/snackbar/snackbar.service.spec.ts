4e443554c9fef8c3f0278354d12c4acc834e6b13
Alfred Galibois-Bouchard <alfredgaliboisb@gmail.com>
2025-02-10 22:25:17 -0500

    snackbar test done

--- client/src/app/services/snackbar/snackbar.service.spec.ts
+++ client/src/app/services/snackbar/snackbar.service.spec.ts
@@ -2,18 +2,23 @@ import { TestBed } from '@angular/core/testing';
 import { MatSnackBar, MatSnackBarModule, MatSnackBarRef } from '@angular/material/snack-bar';
 import { ConfirmComponent } from '@app/components/confirm/confirm.component';
 import { SNACKBAR_CONFIG } from '@app/constants/global.constants';
-import { of } from 'rxjs';
+import { Subject } from 'rxjs';
 import { SnackbarService } from './snackbar.service';
 
 describe('SnackbarService', () => {
     let service: SnackbarService;
     let snackBarSpy: jasmine.SpyObj<MatSnackBar>;
     let snackBarRefMock: jasmine.SpyObj<MatSnackBarRef<unknown>>;
+    let dismissSubject: Subject<{ dismissedByAction: boolean }>;
+    let actionSubject: Subject<void>;
 
     beforeEach(() => {
+        dismissSubject = new Subject();
+        actionSubject = new Subject();
+
         snackBarRefMock = jasmine.createSpyObj('MatSnackBarRef', ['afterDismissed', 'onAction', 'dismiss']);
-        snackBarRefMock.afterDismissed.and.returnValue(of({ dismissedByAction: false }));
-        snackBarRefMock.onAction.and.returnValue(of(undefined));
+        snackBarRefMock.afterDismissed.and.returnValue(dismissSubject.asObservable());
+        snackBarRefMock.onAction.and.returnValue(actionSubject.asObservable());
 
         snackBarSpy = jasmine.createSpyObj('MatSnackBar', ['open', 'openFromComponent']);
         snackBarSpy.openFromComponent.and.returnValue(snackBarRefMock);
@@ -64,27 +69,23 @@ describe('SnackbarService', () => {
         it('should emit false when snackbar is dismissed without action', (done) => {
             const message = 'Are you sure?';
 
-            snackBarRefMock.afterDismissed.and.returnValue(of({ dismissedByAction: false }));
-
             service.showConfirmation(message).subscribe((result) => {
                 expect(result).toBeFalse();
                 done();
             });
 
-            snackBarRefMock.afterDismissed().subscribe();
+            dismissSubject.next({ dismissedByAction: false });
         });
 
         it('should emit true when action is taken', (done) => {
             const message = 'Are you sure?';
-
-            snackBarRefMock.onAction.and.returnValue(of(undefined));
 
             service.showConfirmation(message).subscribe((result) => {
                 expect(result).toBeTrue();
                 done();
             });
 
-            snackBarRefMock.onAction().subscribe();
+            actionSubject.next();
         });
     });
 });
