d5d27595df05545e4b48582e269621bd0e11afc5
R3ivil <olivierohara@outlook.com>
2025-02-25 10:39:22 -0500

    feat: generateCode

--- client/src/app/services/room-validation/room-validation.service.ts
+++ client/src/app/services/room-validation/room-validation.service.ts
@@ -1,23 +1,38 @@
 import { Injectable } from '@angular/core';
+import { ACCESS_CODE_MIN_VALUE, ACCESS_CODE_RANGE } from '@app/constants/global.constants';
 
 @Injectable({
     providedIn: 'root',
 })
 export class RoomValidationService {
-    // constructor(private readonly socketClientService: SocketClientService) {}
-    currentAcessCode: string = '';
+    private codes: string[] = [];
+    private currentAccessCode: string = '';
 
-    validateCode(code: string) {
-        this.isAccessCodeValid(code);
+    validateCode(code: string): boolean {
+        if (!this.containsCode(code)) {
+            return false;
+        }
         this.isGameUnlock();
-        this.currentAcessCode = code;
+        this.currentAccessCode = code;
+        return true;
     }
 
     private isGameUnlock(): boolean {
+        // Add logic if needed
         return true;
     }
 
-    private isAccessCodeValid(code: string): boolean {
-        return true;
+    private containsCode(code: string): boolean {
+        return this.codes.includes(code);
+    }
+
+    private generateAccessCode(): void {
+        this.currentAccessCode = Math.floor(ACCESS_CODE_MIN_VALUE + Math.random() * ACCESS_CODE_RANGE).toString();
+
+        if (this.containsCode(this.currentAccessCode)) {
+            this.generateAccessCode();
+        } else {
+            this.codes.push(this.currentAccessCode);
+        }
     }
 }
