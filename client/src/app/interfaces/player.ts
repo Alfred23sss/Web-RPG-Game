a3e5181c90064fe66fedfa818194395cc4d14ebe
Alfred Galibois-Bouchard <alfredgaliboisb@gmail.com>
2025-03-06 14:44:25 -0500

    feat: fix player bug

--- client/src/app/interfaces/player.ts
+++ client/src/app/interfaces/player.ts
@@ -14,18 +14,3 @@ export interface Player {
     inventory: [Item | null, Item | null];
     isAdmin: boolean;
 }
-
-export interface PlayerInfo {
-    name: string;
-    avatar: string;
-    speed: number;
-    attack: { value: number; bonusDice: DiceType };
-    defense: { value: number; bonusDice: DiceType };
-    hp: { current: number; max: number };
-    movementPoints: number;
-    actionPoints: number;
-    inventory: [Item | null, Item | null];
-    isAdmin: boolean;
-}
-
-// snip
