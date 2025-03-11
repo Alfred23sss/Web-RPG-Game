52a873d404b5056838f9739f68f1851322dc8289
R3ivil <olivierohara@outlook.com>
2025-03-11 11:37:30 -0400

    feat: added no lint

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -38,6 +38,8 @@ export class GamePageComponent implements OnInit, OnDestroy {
     activeTab: 'chat' | 'log' = 'chat';
     logBookSubscription: Subscription;
 
+    //nous avons besoin de ces 6 services pour que tout fonctionne.
+    // eslint-disable-next-line max-params
     constructor(
         // private gameService: GameService,
         private gridService: GridService,
