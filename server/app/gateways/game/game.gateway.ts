d430de78ded7b9eaed959e8e9a07ba04832cb52f
insainteo <teosain11@gmail.com>
2025-03-18 01:46:07 -0400

    fix: update unavailable avatars when game ends

--- server/app/gateways/game/game.gateway.ts
+++ server/app/gateways/game/game.gateway.ts
@@ -50,7 +50,9 @@ export class GameGateway {
             this.gameSessionService.deleteGameSession(payload.accessCode);
             this.accessCodesService.removeAccessCode(payload.accessCode);
             this.server.to(payload.accessCode).emit('gameDeleted');
+            this.server.to(payload.accessCode).emit('updateUnavailableOptions', { avatars: [] });
         }
+        this.server.to(client.id).emit('updateUnavailableOptions', { avatars: [] });
         this.server.to(payload.accessCode).emit('game-abandoned', { player: playerAbandon });
         this.logger.log('game abandoned emitted');
     }
@@ -227,7 +229,7 @@ export class GameGateway {
 
         this.gameSessionService.deleteGameSession(payload.accessCode);
         this.server.to(payload.accessCode).emit('gameEnded', { winner: payload.winner });
-
+        this.server.to(payload.accessCode).emit('updateUnavailableOptions', { avatars: [] });
         const lobbyPlayers = this.lobbyService.getLobbyPlayers(payload.accessCode);
         this.lobbyService.clearLobby(payload.accessCode);
         for (const player of lobbyPlayers) {
