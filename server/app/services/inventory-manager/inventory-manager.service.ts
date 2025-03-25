efb4b553ee3840fb61cb7254a9b7003410363165
W <william.perron@polymtl.ca>
2025-03-25 16:05:06 -0400

    feat : inventory manager plan

--- server/app/services/inventory-manager/inventory-manager.service.ts
+++ server/app/services/inventory-manager/inventory-manager.service.ts
@@ -1,6 +1,39 @@
+import { Tile } from '@app/interfaces/Tile';
+import { Item } from '@app/model/database/item';
+import { Player } from '@app/model/database/player';
 import { Injectable } from '@nestjs/common';
+import { GridManagerService } from '../grid-manager/grid-manager.service';
 
 @Injectable()
 export class InventoryManagerService {
-    constructor() {}
+    constructor(private readonly gridManager: GridManagerService) {}
+
+    //ajouter item a player
+    addItemToPlayer(grid: Tile[][], player: Player): void {
+        let tile = this.gridManager.findTileByPlayer(grid, player);
+        for (const item of player.inventory) {
+            if (item) {
+                // ajouter a l'inventaire
+                // emit success
+                tile.item = undefined;
+                return;
+            }
+        }
+        // call items chose
+
+        //this.emitGridUpdate(accessCode, gameSession.game.grid);
+    }
+
+    // choisir item???
+    removeChosenItem(grid: Tile[][], player: Player, item: Item): void {
+        // déterminer item a jeter
+        // remplacer inventaire au besoin
+        // placer bon item sur la grid
+    }
+
+    // dropper toute les items
+    dropAllItems(grid: Tile[][], player: Player): void {
+        //pour i item de inventaire pas null
+        //trouver tile la plus proche incluant ou il se trouve et placer un objet dessus
+    }
 }
