6c3456a539602e41e89a5c5a216fbf93b50848e6
TheLordSpoty <louisfelixbellemare@gmail.com>
2025-03-25 11:08:56 -0400

    fix: lint errors

--- client/src/app/components/log-book/log-book.component.spec.ts
+++ client/src/app/components/log-book/log-book.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LogBookComponent } from './log-book.component';
 
 describe('LogBookComponent', () => {
-  let component: LogBookComponent;
-  let fixture: ComponentFixture<LogBookComponent>;
+    let component: LogBookComponent;
+    let fixture: ComponentFixture<LogBookComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [LogBookComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [LogBookComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(LogBookComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(LogBookComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
