8c437a62efacfc71335d17863b799cdbb1069976
Teo Sain <teosain11@gmail.com>
2025-03-26 13:51:34 -0400

    test: fixed client side test for player-info and socket-listener

--- client/src/app/components/player-info/player-info.component.spec.ts
+++ client/src/app/components/player-info/player-info.component.spec.ts
@@ -1,77 +1,78 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { GameData } from '@app/classes/gameData';
-import { GameCombatComponent } from '@app/components/game-combat/game-combat.component';
-import { Player } from '@app/interfaces/player';
 import { GameStateSocketService } from '@app/services/game-state-socket/game-state-socket.service';
-import { GameplayService } from '@app/services/gameplay/gameplay.service';
-import { of } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
+import { PlayerInfoComponent } from './player-info.component';
 
-describe('GameCombatComponent', () => {
-    let component: GameCombatComponent;
-    let fixture: ComponentFixture<GameCombatComponent>;
-    let mockDialogRef: jasmine.SpyObj<MatDialogRef<GameCombatComponent>>;
-    let mockGameStateService: jasmine.SpyObj<GameStateSocketService>;
-    let mockGameplayService: jasmine.SpyObj<GameplayService>;
-    let mockGameData: GameData;
-    let mockAttacker: Player;
-    let mockDefender: Player;
+describe('PlayerInfoComponent', () => {
+    let component: PlayerInfoComponent;
+    let fixture: ComponentFixture<PlayerInfoComponent>;
+    let gameDataSubject: BehaviorSubject<GameData>;
 
     beforeEach(async () => {
-        mockGameData = new GameData();
-        mockAttacker = {} as Player;
-        mockDefender = {} as Player;
+        const initialGameData = new GameData();
+        gameDataSubject = new BehaviorSubject<GameData>(initialGameData);
 
-        mockDialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
-        mockGameStateService = jasmine.createSpyObj('GameStateSocketService', [], {
-            gameData$: of(mockGameData),
-            closePopup$: of(undefined),
+        const spy = jasmine.createSpyObj('GameStateSocketService', ['updateGameData'], {
+            gameData$: gameDataSubject.asObservable(),
+            gameDataSubjectValue: initialGameData,
         });
-        mockGameplayService = jasmine.createSpyObj('GameplayService', ['attack', 'evade']);
 
         await TestBed.configureTestingModule({
-            declarations: [GameCombatComponent],
+            declarations: [PlayerInfoComponent],
             providers: [
-                { provide: MatDialogRef, useValue: mockDialogRef },
-                { provide: GameStateSocketService, useValue: mockGameStateService },
-                { provide: GameplayService, useValue: mockGameplayService },
-                { provide: MAT_DIALOG_DATA, useValue: { gameData: mockGameData, attacker: mockAttacker, defender: mockDefender } },
+                {
+                    provide: GameStateSocketService,
+                    useValue: spy,
+                },
             ],
         }).compileComponents();
 
-        fixture = TestBed.createComponent(GameCombatComponent);
+        TestBed.inject(GameStateSocketService) as jasmine.SpyObj<GameStateSocketService>;
+        fixture = TestBed.createComponent(PlayerInfoComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
     });
 
-    it('should subscribe to gameData$', () => {
+    it('should subscribe to gameData$ on construction', () => {
+        const mockGameData = new GameData();
+        mockGameData.lobby.players = [];
+
+        gameDataSubject.next(mockGameData);
+        fixture.detectChanges();
+
         expect(component.gameData).toEqual(mockGameData);
     });
 
-    it('should call attack method on onAttack', () => {
-        component.onAttack();
-        expect(mockGameplayService.attack).toHaveBeenCalledWith(mockGameData);
-    });
+    it('should unsubscribe from gameData$ on destroy', () => {
+        const subscriptionSpy = spyOn(component['gameDataSubscription'], 'unsubscribe');
 
-    it('should call evade method on onEvade', () => {
-        component.onEvade();
-        expect(mockGameplayService.evade).toHaveBeenCalledWith(mockGameData);
-    });
-
-    it('should close dialog on onClose', () => {
-        component.onClose();
-        expect(mockDialogRef.close).toHaveBeenCalled();
-    });
-
-    it('should unsubscribe on destroy', () => {
-        spyOn(component['gameDataSubscription'], 'unsubscribe');
-        spyOn(component['closePopupSubscription'], 'unsubscribe');
         component.ngOnDestroy();
-        expect(component['gameDataSubscription'].unsubscribe).toHaveBeenCalled();
-        expect(component['closePopupSubscription'].unsubscribe).toHaveBeenCalled();
+
+        expect(subscriptionSpy).toHaveBeenCalled();
+    });
+
+    it('should handle initial gameData correctly', () => {
+        const initialGameData = new GameData();
+
+        fixture.detectChanges();
+
+        expect(component.gameData).toEqual(initialGameData);
+    });
+
+    it('should update gameData when new data is emitted', () => {
+        const initialGameData = new GameData();
+        const updatedGameData = new GameData();
+        updatedGameData.lobby.players = [];
+
+        gameDataSubject.next(initialGameData);
+        fixture.detectChanges();
+        gameDataSubject.next(updatedGameData);
+        fixture.detectChanges();
+
+        expect(component.gameData).toEqual(updatedGameData);
     });
 });
--- client/src/app/services/socket-listener/socket-listener.service.spec.ts
+++ client/src/app/services/socket-listener/socket-listener.service.spec.ts
@@ -1,5 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { EVENTS } from '@app/constants/global.constants';
+import { AccessCodesCommunicationService } from '@app/services/access-codes-communication/access-codes-communication.service';
 import { CombatSocketService } from '@app/services/combat-socket/combat-socket.service';
 import { GameSocketService } from '@app/services/game-socket/game-socket.service';
 import { GameStateSocketService } from '@app/services/game-state-socket/game-state-socket.service';
@@ -7,39 +9,66 @@ import { SocketClientService } from '@app/services/socket/socket-client-service'
 import { TurnSocketService } from '@app/services/turn-socket/turn-socket.service';
 import { SocketListenerService } from './socket-listener.service';
 
+// Create a test-specific subclass to expose the method for testing
+class TestSocketListenerService extends SocketListenerService {
+    testUnsubscribeSocketListeners(events: string[]): void {
+        events.forEach((event) => {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            (this as any).socketClientService.off(event);
+        });
+    }
+}
 describe('SocketListenerService', () => {
-    let service: SocketListenerService;
-    let mockGameStateService: jasmine.SpyObj<GameStateSocketService>;
-    let mockCombatService: jasmine.SpyObj<CombatSocketService>;
-    let mockTurnService: jasmine.SpyObj<TurnSocketService>;
-    let mockGameSocketService: jasmine.SpyObj<GameSocketService>;
-    let mockSocketClientService: jasmine.SpyObj<SocketClientService>;
+    let service: TestSocketListenerService;
+    let gameStateServiceSpy: jasmine.SpyObj<GameStateSocketService>;
+    let combatSocketServiceSpy: jasmine.SpyObj<CombatSocketService>;
+    let turnSocketServiceSpy: jasmine.SpyObj<TurnSocketService>;
+    let gameSocketServiceSpy: jasmine.SpyObj<GameSocketService>;
+    let socketClientServiceSpy: jasmine.SpyObj<SocketClientService>;
 
     beforeEach(() => {
-        const socketMock = jasmine.createSpyObj('socket', ['on', 'off']);
-
-        const socketClientServiceSpy = jasmine.createSpyObj('SocketClientService', [], {
-            socket: socketMock,
-        });
-
-        // Mock other services as usual
-        mockGameStateService = jasmine.createSpyObj('GameStateSocketService', ['initializeListeners']);
-        mockCombatService = jasmine.createSpyObj('CombatSocketService', ['initializeCombatListeners']);
-        mockTurnService = jasmine.createSpyObj('TurnSocketService', ['initializeTurnListeners']);
-        mockGameSocketService = jasmine.createSpyObj('GameSocketService', ['initializeSocketListeners']);
+        const createSpyObj = (methods: string[]) => jasmine.createSpyObj('Service', methods);
 
         TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
             providers: [
-                SocketListenerService,
-                { provide: GameStateSocketService, useValue: mockGameStateService },
-                { provide: CombatSocketService, useValue: mockCombatService },
-                { provide: TurnSocketService, useValue: mockTurnService },
-                { provide: GameSocketService, useValue: mockGameSocketService },
-                { provide: SocketClientService, useValue: socketClientServiceSpy },
+                {
+                    provide: SocketListenerService,
+                    useClass: TestSocketListenerService,
+                },
+                {
+                    provide: GameStateSocketService,
+                    useValue: createSpyObj(['initializeListeners']),
+                },
+                {
+                    provide: CombatSocketService,
+                    useValue: createSpyObj(['initializeCombatListeners']),
+                },
+                {
+                    provide: TurnSocketService,
+                    useValue: createSpyObj(['initializeTurnListeners']),
+                },
+                {
+                    provide: GameSocketService,
+                    useValue: createSpyObj(['initializeSocketListeners']),
+                },
+                {
+                    provide: SocketClientService,
+                    useValue: createSpyObj(['off']),
+                },
+                {
+                    provide: AccessCodesCommunicationService,
+                    useValue: createSpyObj(['validateAccessCode', 'removeAccessCode']),
+                },
             ],
         });
 
-        service = TestBed.inject(SocketListenerService);
+        service = TestBed.inject(SocketListenerService) as TestSocketListenerService;
+        gameStateServiceSpy = TestBed.inject(GameStateSocketService) as jasmine.SpyObj<GameStateSocketService>;
+        combatSocketServiceSpy = TestBed.inject(CombatSocketService) as jasmine.SpyObj<CombatSocketService>;
+        turnSocketServiceSpy = TestBed.inject(TurnSocketService) as jasmine.SpyObj<TurnSocketService>;
+        gameSocketServiceSpy = TestBed.inject(GameSocketService) as jasmine.SpyObj<GameSocketService>;
+        socketClientServiceSpy = TestBed.inject(SocketClientService) as jasmine.SpyObj<SocketClientService>;
     });
 
     it('should be created', () => {
@@ -47,25 +76,31 @@ describe('SocketListenerService', () => {
     });
 
     describe('initializeAllSocketListeners', () => {
-        it('should initialize all service listeners', () => {
+        it('should initialize listeners for all socket services', () => {
             service.initializeAllSocketListeners();
 
-            expect(mockGameStateService.initializeListeners).toHaveBeenCalled();
-            expect(mockCombatService.initializeCombatListeners).toHaveBeenCalled();
-            expect(mockTurnService.initializeTurnListeners).toHaveBeenCalled();
-            expect(mockGameSocketService.initializeSocketListeners).toHaveBeenCalled();
+            expect(gameStateServiceSpy.initializeListeners).toHaveBeenCalled();
+            expect(combatSocketServiceSpy.initializeCombatListeners).toHaveBeenCalled();
+            expect(turnSocketServiceSpy.initializeTurnListeners).toHaveBeenCalled();
+            expect(gameSocketServiceSpy.initializeSocketListeners).toHaveBeenCalled();
         });
     });
 
     describe('unsubscribeSocketListeners', () => {
-        it('should unsubscribe from all socket events', () => {
+        it('should call off method for each event in EVENTS', () => {
             service.unsubscribeSocketListeners();
 
             EVENTS.forEach((event) => {
-                expect(mockSocketClientService.socket.off).toHaveBeenCalledWith(event);
+                expect(socketClientServiceSpy.off).toHaveBeenCalledWith(event);
             });
 
-            expect(mockSocketClientService.socket.off).toHaveBeenCalledTimes(EVENTS.length);
+            expect(socketClientServiceSpy.off).toHaveBeenCalledTimes(EVENTS.length);
+        });
+
+        it('should handle empty EVENTS scenario', () => {
+            service.testUnsubscribeSocketListeners([]);
+
+            expect(socketClientServiceSpy.off).not.toHaveBeenCalled();
         });
     });
 });
