4cedc62ec6b8ec28fc1aea5ba52e05cc5f53e371
Teo Sain <teosain11@gmail.com>
2025-03-27 18:51:13 -0400

    feat: new textures added to assets

--- client/src/app/enums/global.enums.ts
+++ client/src/app/enums/global.enums.ts
@@ -45,12 +45,12 @@ export enum AvatarType {
 }
 
 export enum ImageType {
-    Water = './assets/tile-items/water.png',
-    Wall = './assets/tile-items/wall.png',
-    OpenDoor = './assets/tile-items/opened-door.png',
-    ClosedDoor = './assets/tile-items/closed-door.png',
-    Ice = './assets/tile-items/ice.png',
-    Default = './assets/tile-items/clay.png',
+    Water = './assets/tile-items/waterGif.gif',
+    Wall = './assets/tile-items/tileset-9-tiles-example.png',
+    OpenDoor = './assets/tile-items/door-openedA.png',
+    ClosedDoor = './assets/tile-items/door-closedA.png',
+    Ice = './assets/tile-items/iceA.PNG',
+    Default = './assets/tile-items/floorB.PNG',
 }
 
 export enum ItemType {
