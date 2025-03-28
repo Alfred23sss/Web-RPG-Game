2bacc03b9d32e3f46bd5ba00651e0fab1b2e9edd
Teo Sain <teosain11@gmail.com>
2025-03-28 17:34:02 -0400

    feat: new design for game-info component

--- client/src/app/components/game-info/game-info.component.ts
+++ client/src/app/components/game-info/game-info.component.ts
@@ -1,19 +1,13 @@
 import { DatePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { Game } from '@app/interfaces/game';
 
 @Component({
     selector: 'app-game-info',
     standalone: true,
-    imports: [DatePipe],
-    template: `
-        <div class="game-info">
-            <h3>{{ game.name }}</h3>
-            <p>Taille: {{ game.size }}x{{ game.size }}</p>
-            <p>Mode de jeu: {{ game.mode }}</p>
-            <p>Dernière modification: {{ game.lastModified | date: 'short' }}</p>
-        </div>
-    `,
+    imports: [DatePipe, MatTooltipModule],
+    templateUrl: './game-info.component.html',
     styleUrls: ['./game-info.component.scss'],
 })
 export class GameInfoComponent {
