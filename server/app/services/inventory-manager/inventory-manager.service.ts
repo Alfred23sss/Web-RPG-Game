5efdb8ed3a66a1981496bb5a956de518c9635a13
R3ivil <olivierohara@outlook.com>
2025-03-27 21:04:32 -0400

    feat: players can now pick up items, but they are  reset at the start of the turn...

--- server/app/services/inventory-manager/inventory-manager.service.ts
+++ server/app/services/inventory-manager/inventory-manager.service.ts
@@ -1,70 +1,49 @@
-import { Injectable } from '@nestjs/common';
-import { GameSessionService } from '../game-session/game-session.service';
-import { GridManagerService } from '../grid-manager/grid-manager.service';
+// import { Injectable } from '@nestjs/common';
+// import { GridManagerService } from '@app/services/grid-manager/grid-manager.service';
+// import { GameSession } from '@app/interfaces/GameSession';
+// import { Player } from '@app/interfaces/Player';
+// import { Item } from '@app/interfaces/Item';
 
-@Injectable()
-export class InventoryManagerService {
-    constructor(
-        private readonly gridManager: GridManagerService,
-        private readonly gameSession: GameSessionService,
-    ) {}
+// @Injectable()
+// export class InventoryManagerService {
+//     constructor(private readonly gridManager: GridManagerService) {}
 
-    //ajouter item a player
-    addItemToPlayer(accessCode: string, playerName: string): void {
-        const gameSession = this.gameSession.getGameSession(accessCode);
-        const player = gameSession.turn.orderedPlayers.find((p) => p.name === playerName);
-        let tile = this.gridManager.findTileByPlayer(gameSession.game.grid, player);
+//     // choisir item???
+//     removeChosenItem(accessCode: string, playerName: string, item: string): void {
+//         const gameSession = this.gameSession.getGameSession(accessCode);
+//         const player = gameSession.turn.orderedPlayers.find((p) => p.name === playerName);
+//         const tile = this.gridManager.findTileByPlayer(gameSession.game.grid, player);
 
-        if (tile.item) {
-            for (let i = 0; i < player.inventory.length; i++) {
-                if (!player.inventory[i]) {
-                    player.inventory[i] = tile.item;
-                    tile.item = undefined;
-                    this.gameSession.updatePlayer(player, { inventory: player.inventory });
-                    this.gameSession.emitGridUpdate(accessCode, gameSession.game.grid);
-                    return;
-                }
-            }
-        }
-        // call items chose
-    }
+//         if (tile.item?.name === item) {
+//             return;
+//         }
 
-    // choisir item???
-    removeChosenItem(accessCode: string, playerName: string, item: string): void {
-        const gameSession = this.gameSession.getGameSession(accessCode);
-        const player = gameSession.turn.orderedPlayers.find((p) => p.name === playerName);
-        let tile = this.gridManager.findTileByPlayer(gameSession.game.grid, player);
+//         for (let i = 0; i < player.inventory.length; i++) {
+//             if (player.inventory[i]?.name === item) {
+//                 tile.item = player.inventory[i];
+//                 player.inventory[i] = null;
+//                 this.gameSession.updatePlayer(player, { inventory: player.inventory });
+//                 this.gameSession.emitGridUpdate(accessCode, gameSession.game.grid);
+//             }
+//         }
+//         return;
+//     }
 
-        if (tile.item?.name === item) {
-            return;
-        }
+//     // dropper toute les items
+//     dropAllItems(accessCode: string, playerName: string): void {
+//         const gameSession = this.gameSession.getGameSession(accessCode);
+//         const player = gameSession.turn.orderedPlayers.find((p) => p.name === playerName);
+//         const tile = this.gridManager.findTileByPlayer(gameSession.game.grid, player);
 
-        for (let i = 0; i < player.inventory.length; i++) {
-            if (player.inventory[i]?.name === item) {
-                tile.item = player.inventory[i];
-                player.inventory[i] = null;
-                this.gameSession.updatePlayer(player, { inventory: player.inventory });
-                this.gameSession.emitGridUpdate(accessCode, gameSession.game.grid);
-            }
-        }
-        return;
-    }
-
-    // dropper toute les items
-    dropAllItems(accessCode: string, playerName: string): void {
-        const gameSession = this.gameSession.getGameSession(accessCode);
-        const player = gameSession.turn.orderedPlayers.find((p) => p.name === playerName);
-        let tile = this.gridManager.findTileByPlayer(gameSession.game.grid, player);
-
-        for (let i = 0; i < player.inventory.length; i++) {
-            if (player.inventory[i]) {
-                //tile.item = player.inventory[i];
-                player.inventory[i] = null;
-            }
-        }
-        this.gameSession.updatePlayer(player, { inventory: player.inventory });
-        this.gameSession.emitGridUpdate(accessCode, gameSession.game.grid);
-        return;
-        //trouver tile la plus proche incluant ou il se trouve et placer un objet dessus
-    }
-}
+//         for (let i = 0; i < player.inventory.length; i++) {
+//             if (player.inventory[i]) {
+//                 tile.item = player.inventory[i];
+//                 player.inventory[i] = null;
+//             }
+//         }
+//         this.gameSession.updatePlayer(player, { inventory: player.inventory });
+//         this.gameSession.emitGridUpdate(accessCode, gameSession.game.grid);
+//         return;
+//         // trouver tile la plus proche incluant ou il se trouve et placer un objet dessus
+//     }
+// }
