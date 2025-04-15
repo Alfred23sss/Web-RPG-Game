d1273789b9370c866c23e68747d765a6744498fa
W <william.perron@polymtl.ca>
2025-04-15 14:16:42 -0400

    test : test player-info

--- client/src/app/components/player-info/player-info.component.spec.ts
+++ client/src/app/components/player-info/player-info.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GameData } from '@app/classes/game-data/game-data';
 import { GameStateSocketService } from '@app/services/game-state-socket/game-state-socket.service';
+import { ItemName } from '@common/enums';
 import { BehaviorSubject } from 'rxjs';
 import { PlayerInfoComponent } from './player-info.component';
 
@@ -40,5 +41,36 @@ describe('PlayerInfoComponent', () => {
         const subscriptionSpy = spyOn(component['gameDataSubscription'], 'unsubscribe');
         component.ngOnDestroy();
         expect(subscriptionSpy).toHaveBeenCalled();
+    });
+
+    describe('hasGreatShield', () => {
+        it('should return false if clientPlayer or inventory is missing', () => {
+            component.gameData.clientPlayer = undefined as any;
+            expect(component.hasGreatShield()).toBeFalse();
+
+            component.gameData.clientPlayer = { inventory: undefined } as any;
+            expect(component.hasGreatShield()).toBeFalse();
+        });
+
+        it('should return false if GreatShield is not in inventory', () => {
+            component.gameData.clientPlayer = {
+                inventory: [{ name: ItemName.BlackSword }, { name: ItemName.Armor }],
+            } as any;
+            expect(component.hasGreatShield()).toBeFalse();
+        });
+
+        it('should return true if GreatShield is in inventory', () => {
+            component.gameData.clientPlayer = {
+                inventory: [{ name: ItemName.BlackSword }, { name: ItemName.GreatShield }, { name: ItemName.Armor }],
+            } as any;
+            expect(component.hasGreatShield()).toBeTrue();
+        });
+
+        it('should skip null or undefined items in inventory', () => {
+            component.gameData.clientPlayer = {
+                inventory: [null, undefined, { name: ItemName.GreatShield }],
+            } as any;
+            expect(component.hasGreatShield()).toBeTrue();
+        });
     });
 });
